80382730b178d3f90100fbc1152a08817339150a
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-11-30 12:00:31 -0500

    Initial Commit

---
+++ Matchismo/FWSViewController.m
@@ -0,0 +1,29 @@
+//
+//  FWSViewController.m
+//  Matchismo
+//
+//  Created by abruzzim on 11/30/14.
+//  Copyright (c) 2014 Framework Solutions, LLC. All rights reserved.
+//
+
+#import "FWSViewController.h"
+
+@interface FWSViewController ()
+
+@end
+
+@implementation FWSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MatchismoTests/MatchismoTests.h
@@ -0,0 +1,13 @@
+//
+//  MatchismoTests.h
+//  MatchismoTests
+//
+//  Created by abruzzim on 11/30/14.
+//  Copyright (c) 2014 Framework Solutions, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MatchismoTests : SenTestCase
+
+@end
